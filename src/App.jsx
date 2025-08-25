ecbf2de4adc57d7ac69162a949dc7ba3dc32ebaa
nabeelnaeem <nnabeel03@gmail.com>
2025-08-25 11:55:09 +0500

    feature(sidebar): Added sidebar and setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import OverviewPage from './pages/OverviewPage';
+import ProductsPage from './pages/ProductsPage';
 
-function BadExample({ name }) {
+function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <h1 className="text-4xl font-bold underline">Hello world!</h1>{' '}
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div className=" absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-700 to-gray-900 opacity-80" />
+        <div className=" absolute inset-0 backdrop-blur-sm" />
+      </div>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/overview" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </div>
   );
 }
 
-BadExample.propTypes = {
-  name: PropTypes.string.isRequired,
-};
-
-export default BadExample;
+export default App;
---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OverviewPage() {
+  return <div>Overview page</div>;
+}
+
+export default OverviewPage;
