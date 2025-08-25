8a5fe006792a9b43908657bf71f47654a78f938e
nabeelnaeem <nnabeel03@gmail.com>
2025-08-25 15:35:28 +0500

    feature(orders-page): Updated order page

---
+++ src/components/orders/DailyOrders.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DailyOrders() {
+  return <div>DailyOrder</div>;
+}
+
+export default DailyOrders;
---
+++ src/components/orders/OrderDistribution.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OrderDistribution() {
+  return <div>OrderDistribution</div>;
+}
+
+export default OrderDistribution;
