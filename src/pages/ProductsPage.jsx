c638d7e750ef9b5428bf6fbe0c544259eff4bf9d
nabeelnaeem <nnabeel03@gmail.com>
2025-08-25 14:36:54 +0500

    feature(products-page): Added and updated producsPage and related components

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { AlertTriangle, DollarSign, Package, TrendingUp } from 'lucide-react';
+import Header from '../components/common/Header';
+import StatCard from '../components/common/StatCard';
+import ProductsTable from '../components/products/ProductsTable';
+import CategoryDistributionChart from '../components/overview/CategoryDistributionChart';
+import SalesTrendChart from '../components/products/SalesTrendChart';
 
 function ProductsPage() {
-  return <div>Products Page</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Products" />
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8 xl:px-20">
+        {/* Stats */}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard name="Total Products" icon={Package} value="$12,345" color="#6366F1" />
+          <StatCard name="Top Selling" icon={TrendingUp} value="1,234" color="#8B5CF6" />
+          <StatCard name="Low Stock" icon={AlertTriangle} value="567" color="#EC4899" />
+          <StatCard name="Total Revenue" icon={DollarSign} value="12.5%" color="#10B981" />{' '}
+        </motion.div>
+        <ProductsTable />
+        {/* Charts */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <SalesTrendChart />
+          <CategoryDistributionChart />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default ProductsPage;
